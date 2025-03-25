e21702c6afc41fa9475b185b60b2a10ba64f35a8
Samir Das <samircd4@gmail.com>
2025-03-26 01:57:03 +0600

    headless_true

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 
 def go_to_google():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://www.google.com")
         page.screenshot(path="google.png")
