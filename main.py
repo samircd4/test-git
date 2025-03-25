15126baf89d858b9e2f45f7361dcfb740cbca748
Samir Das <samircd4@gmail.com>
2025-03-26 00:32:49 +0600

    test commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+
+
+def go_to_google():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        page.screenshot(path="google.png")
+        page.wait_for_timeout(50000)
+        browser.close()
+
+
+if __name__ == '__main__':
+    go_to_google()
